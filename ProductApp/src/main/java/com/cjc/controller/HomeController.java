f1b7d3b223be3eb8d3b3901f8a19d9b53ca657f0
AnkitMahant <ankitmahant786@gmail.com>
2024-12-05 13:27:26 +0530

    Merge with Developer1 commit

--- ProductApp/src/main/java/com/cjc/controller/HomeController.java
+++ ProductApp/src/main/java/com/cjc/controller/HomeController.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController
 {
-@postMapping("/save-Product)
+@PostMapping ("/save-Product")
 public String saveProduct()
 	{
 
