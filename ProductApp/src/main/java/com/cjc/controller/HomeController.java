54ebcec8c4a72651124a214de7e3ca9c25cd8c2e
AnkitMahant <ayaanofficial103@gmail.com>
2024-12-05 13:11:47 +0530

    Developer1 commit

--- ProductApp/src/main/java/com/cjc/controller/HomeController.java
+++ ProductApp/src/main/java/com/cjc/controller/HomeController.java
@@ -5,7 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController
 {
-	
+@postMapping("/save-Product)
+public String saveProduct()
+	{
+
+return "product will be save later";
+		
+	}
 	
 
 }
